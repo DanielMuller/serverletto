4225a4345989514dadeacd7e4649b93226d0b738
Daniel Muller <daniel.muller@serverlessguru.com>
2023-05-18 14:16:40 +0100

    chore(frontend): stages

--- frontend/src/aws-exports.dev.js
+++ frontend/src/aws-exports.dev.js
@@ -11,20 +11,3 @@ const awsmobile = {
 };
 
 export default awsmobile;
-
-/**
- * Prod
- * /
-/*
-{
-  aws_project_region: 'eu-west-3',
-  aws_cognito_region: 'eu-west-3',
-  aws_user_pools_id: 'eu-west-3_xxxxx',
-  aws_user_pools_web_client_id: 'xxxxx',
-  oauth: {},
-  aws_user_files_s3_bucket: 'serverletto-participants-prod',
-  aws_user_files_s3_bucket_region: 'eu-west-3',
-  api_endpoint: 'api.serverletto.net',
-  domain: 'serverletto.net',
-},
-*/
